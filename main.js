43ece6193a06297974d13f2aef9801352e4f2fcd
Ajmat-Ali <badshah9631619358@gmail.com>
2026-01-18 17:11:12 +0530

    configure AjX Shop

---
+++ main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = React.createElement("h1", null, "Hi This is H1 and AjX Shop");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
